9fb18982d69267b8362c3e2ee7b397d11aa108b8
Leonardo Akiyama <kenji_akiyama@hotmail.com>
2023-11-21 17:27:39 -0300

    primeiro commit

---
+++ Aula10/script.js
@@ -0,0 +1,7 @@
+var var1 = 10;
+let let1 = 5;
+const const1 = 15;
+
+document.getElementById('js').innerText = "Olá Mundo\n";
+
+document.getElementById('js').innerText += "Var = " + var1 + "\nLet: " + let1 + "\nConst: " + const1;
